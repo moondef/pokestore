d26d375a64917df259dd82587b2b591eb3fe68f4
moondef <stpnsamko@gmail.com>
2018-06-27 01:02:02 +0300

    Remove from cart

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -1,9 +1,11 @@
-import { ADD_TO_CART, REMOVE_FROM_CART } from '../actions'
+import { ADD_TO_CART, REMOVE_FROM_CART, CLEAR_CART } from '../actions'
 
 export const reducer = (state = [], action) => {
   switch (action.type) {
     case ADD_TO_CART:
       return addToCart(state, action.payload)
+    case REMOVE_FROM_CART:
+      return removeFromCart(state, action.payload)
   }
 }
 
@@ -19,3 +21,10 @@ const addToCart = (cart, pokemon) => {
 }
 
 const inCart = (cart, pokemon) => cart.some(item => item['name'] === pokemon.name)
+
+// utils for REMOVE_FROM_CART
+const removeFromCart = (cart, pokemon) => {
+  const index = cart.indexOf(cart.find(item => item.name === pokemon.name))
+  cart.splice(index, index + 1)
+  return cart
+}
