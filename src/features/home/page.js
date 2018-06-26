4dfce300a9b3267584ca935e4c5d03c3ee1ef286
moondef <stpnsamko@gmail.com>
2018-06-27 02:10:19 +0300

    Add header molecule

--- src/features/home/page.js
+++ src/features/home/page.js
@@ -1,8 +1,10 @@
 import React from 'react'
 import { Button } from '../../ui'
+import { Header } from '../../ui'
 
 export const HomePage = () => (
   <React.Fragment>
+    <Header />
     <Button>This is a button</Button>
   </React.Fragment>
 )
---
+++ src/ui/molecules/header.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import styled from 'styled-components'
+
+import { Link } from 'react-router-dom'
+import { Container } from '../atoms'
+
+import { color } from '../theme'
+
+export const Header = () => (
+  <HeaderWrapper>
+    <Link to="/">HOME</Link>
+    <Link to="/cart">CART</Link>
+  </HeaderWrapper>
+)
+
+const HeaderWrapper = styled(Container)`
+  align-items: stretch;
+  background-color: white;
+  border-bottom: 1px solid ${color.primary};
+  display: flex;
+  flex-shrink: 0;
+  height: 4rem;
+  justify-content: space-between;
+  align-items: center;
+`
